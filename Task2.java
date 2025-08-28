f7721c26f75f4846b2371cb940b375b831750e29
Btmrj-rawr <beato.mariejo@gmail.com>
2025-08-28 10:28:08 +0000

    Done for today's task.

--- Task2.java
+++ Task2.java
@@ -1,1 +1,16 @@
-
+public class Task2 {
+    public static void main(String[] args) {
+        char h = 'H';
+        short e = 3;
+        byte llo = 110;
+        short o = 0;
+        char r = 'r';
+        char l = 'l';
+        char d = 'd';
+        float two = 2.0f;
+        boolean t = true;
+        String out = "" + h + e + llo;
+        String put = "w" + o + r + l + d;
+        System.out.println(out + put + two + t); 
+    }
+}
